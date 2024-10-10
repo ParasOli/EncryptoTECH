e474af5601e87610ea4e8877cad92647ee2c9059
Paras Oli <146341780+ParasOli@users.noreply.github.com>
2024-10-10 13:19:23 +0545

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,29 +1,28 @@
 // Footer.jsx
 import React from 'react';
 import { FaPhone, FaEnvelope } from 'react-icons/fa';
-import logo from '../assets/company/encrypto.png'; // Update the path to your logo image
+import logo from '../assets/company/encrypto.png'; 
 
 const Footer = () => {
   return (
     <footer className="bg-gray-800 text-white py-6 mt-auto">
-      <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
+      <div className="container mx-auto px-4 flex flex-col md:flex-row justify-between items-center">
         <div className="flex items-center">
-          {/* <img src={logo} alt="Encrypto TECH Logo" className="h-20" /> */}
-          <span className="text-lg font-bold">Encrypto TECH</span>
+          <span className="text-lg font-bold">Encrypto</span>
         </div>
-        <div className="flex items-center mt-4 md:mt-0">
-          <a href="tel:+123456789" className="flex items-center mr-6">
+        <div className="flex flex-col md:flex-row items-center mt-4 md:mt-0 space-y-4 md:space-y-0 md:space-x-6">
+          <a href="tel:+123456789" className="flex items-center text-sm md:text-base">
             <FaPhone className="mr-2" />
-            <span>+123 456 789</span>
+            <span>+977 9868757379</span>
           </a>
-          <a href="mailto:example@mail.com" className="flex items-center">
+          <a href="mailto:example@mail.com" className="flex items-center text-sm md:text-base">
             <FaEnvelope className="mr-2" />
-            <span>example@mail.com</span>
+            <span>encrypto@gmail.com</span>
           </a>
         </div>
       </div>
       <div className="text-center mt-4">
-        <p>&copy; {new Date().getFullYear()} Encrypto TECH. All rights reserved.</p>
+        <p className="text-sm">&copy; {new Date().getFullYear()} Encrypto, All rights reserved.</p>
       </div>
     </footer>
   );
