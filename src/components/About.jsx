bc56076736363e54d101c098548bca1255720f9e
Paras Oli <146341780+ParasOli@users.noreply.github.com>
2024-10-10 13:28:41 +0545

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,7 +10,7 @@ import { fadeIn, textVariant } from "../utils/motion";
 const ServiceCard = ({ index, title, icon }) => (
   <Tilt className='w-full'>
     <motion.div
-      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+      variants={fadeIn("right", "spring", index * 0.1, 0.1)} 
       className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
     >
       <div
@@ -24,7 +24,7 @@ const ServiceCard = ({ index, title, icon }) => (
         <img
           src={icon}
           alt='web-development'
-          className='w-12 h-12 object-contain' // Adjusted icon size for small devices
+          className='w-12 h-12 object-contain' 
         />
 
         <h3 className='text-white text-[18px] font-bold text-center'>
@@ -38,16 +38,16 @@ const ServiceCard = ({ index, title, icon }) => (
 const About = () => {
   return (
     <>
-      <motion.div variants={textVariant()}>
-        <p className={`${styles.sectionSubText} text-[14px] md:text-[16px]`}>Introduction</p> {/* Responsive subtext size */}
-        <h2 className={`${styles.sectionHeadText} text-[20px] md:text-[24px]`}>Overview.</h2> {/* Responsive heading size */}
+      <motion.div variants={textVariant(0.2, 0.5)}>
+        <p className={`${styles.sectionSubText} text-[14px] md:text-[16px]`}>Introduction</p>
+        <h2 className={`${styles.sectionHeadText} text-[20px] md:text-[24px]`}>Overview.</h2>
       </motion.div>
 
       <motion.p
-        variants={fadeIn("", "", 0.1, 1)}
-        className='mt-4 text-secondary text-[14px] md:text-[17px] max-w-3xl leading-[30px]' // Responsive paragraph size
+        variants={fadeIn("", "", 0.05, 0.5)} 
+        className='mt-4 text-secondary text-[14px] md:text-[17px] max-w-3xl leading-[30px]'
       >
-        At Encrypto Tech, we are a team of skilled software developers and digital marketing experts 
+        At Encrypto, we are a team of skilled software developers and digital marketing experts 
         specializing in cybersecurity and web development. With extensive experience across multiple technologies, 
         we deliver innovative solutions. Our quick learning ability and collaborative approach enable us to work 
         closely with clients, creating efficient, scalable, and user-friendly solutions that address real-world challenges. 
